5cccd09f16217763b86fc59ad9a5204d981484b2
Diana Le <lepdiana01@gmail.com>
2019-12-04 15:11:35 -0700

    first commit

---
+++ src/components/Product.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import '../style/product.css'
+
+const Product = props => {
+    const mappedFilteredProducts = props.rProps.filteredProducts.map(filteredProduct => 
+                                                                        <div key={filteredProduct.identifier}>
+                                                                            <h1>{filteredProduct.item.name}</h1>
+                                                                            <p>Description: {filteredProduct.item.description}</p>
+                                                                            <p>Rarity: {filteredProduct.item.rarity}</p>
+                                                                            <p>Cost: {filteredProduct.item.cost} vbucks</p>
+                                                                            <p>Type: {filteredProduct.item.type}</p>
+                                                                            <img src={filteredProduct.item.images.background} alt={filteredProduct.name}/>
+                                                                        </div>
+                                                                    )
+
+    return(
+        <div className='eachProductSection'>
+            {mappedFilteredProducts}
+        </div>
+    )
+}
+
+export default Product
+
+
+
+                                                      
